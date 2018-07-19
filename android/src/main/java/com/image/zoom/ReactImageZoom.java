be069b617c42bbfa0890caa0ca282c4230034286
Ca Phun Ung <caphun@tofugear.com>
2018-07-19 16:24:24 +0800

    removed unused createJSModules calls in 0.47.2+

--- android/src/main/java/com/image/zoom/ReactImageZoom.java
+++ android/src/main/java/com/image/zoom/ReactImageZoom.java
@@ -27,11 +27,6 @@ public class ReactImageZoom implements ReactPackage {
   }
 
   @Override
-  public List<Class<? extends JavaScriptModule>> createJSModules() {
-    return Collections.emptyList();
-  }
-
-  @Override
   public List<ViewManager> createViewManagers(ReactApplicationContext reactApplicationContext) {
       return Arrays.<ViewManager>asList(
           new com.image.zoom.ViewManager(),
